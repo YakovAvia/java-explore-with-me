c1fe4449d756997efa4402a6eb3048819b786b61
YakovAvia <ralfffi@yandex.ru>
2025-11-21 17:16:38 +0300

    1

--- stats-service/stats-server/src/main/java/ru/practicum/stats/server/StatsServerApplication.java
+++ stats-service/stats-server/src/main/java/ru/practicum/stats/server/StatsServerApplication.java
@@ -3,7 +3,7 @@ package ru.practicum.stats.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"ru.practicum.stats.server", "ru.practicum.server.stats"})
 public class StatsServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(StatsServerApplication.class, args);
