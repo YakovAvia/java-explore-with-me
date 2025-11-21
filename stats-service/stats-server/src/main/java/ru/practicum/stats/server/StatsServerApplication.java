03c79df02ed01c2bb767a09c88c4899b4b2a3ac2
YakovAvia <ralfffi@yandex.ru>
2025-11-21 18:56:32 +0300

    1

--- stats-service/stats-server/src/main/java/ru/practicum/stats/server/StatsServerApplication.java
+++ stats-service/stats-server/src/main/java/ru/practicum/stats/server/StatsServerApplication.java
@@ -1,13 +1,34 @@
 package ru.practicum.stats.server;
 
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @SpringBootApplication(scanBasePackages = {"ru.practicum.stats.server", "ru.practicum.server.stats"})
 @EnableJpaRepositories("ru.practicum.server.stats.repository")
 public class StatsServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(StatsServerApplication.class, args);
+    }
+
+    @Configuration
+    public class JacksonConfig {
+
+        @Bean
+        public ObjectMapper objectMapper() {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JavaTimeModule javaTimeModule = new JavaTimeModule();
+            javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+            objectMapper.registerModule(javaTimeModule);
+            return objectMapper;
+        }
     }
 }
