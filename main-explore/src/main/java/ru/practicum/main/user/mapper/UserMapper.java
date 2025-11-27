eb4f3cd637bb7c1e8f11b5a376f5c2790c59ede0
YakovAvia <ralfffi@yandex.ru>
2025-11-27 20:07:46 +0300

    1

--- main-explore/src/main/java/ru/practicum/main/user/mapper/UserMapper.java
+++ main-explore/src/main/java/ru/practicum/main/user/mapper/UserMapper.java
@@ -4,6 +4,7 @@ import ru.practicum.main.user.dto.NewUserRequest;
 import ru.practicum.main.user.dto.UserDto;
 import ru.practicum.main.user.dto.UserShortDto;
 import ru.practicum.main.user.model.User;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,7 +32,7 @@ public class UserMapper {
                 user.getName()
         );
     }
-    
+
     public static List<UserDto> toUserDto(Iterable<User> users) {
         List<UserDto> dtos = new ArrayList<>();
         for (User user : users) {
