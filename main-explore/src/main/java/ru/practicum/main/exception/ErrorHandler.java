966e3b02ea97b3ba3748d55dfda0d46be9128a8e
YakovAvia <ralfffi@yandex.ru>
2025-11-28 20:04:00 +0300

    1

--- main-explore/src/main/java/ru/practicum/main/exception/ErrorHandler.java
+++ main-explore/src/main/java/ru/practicum/main/exception/ErrorHandler.java
@@ -3,6 +3,7 @@ package ru.practicum.main.exception;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -37,16 +38,41 @@ public class ErrorHandler {
         );
     }
 
-
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ApiError handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ApiError handleDataIntegrityViolationException(final ru.practicum.main.exception.DataIntegrityViolationException e) {
         return new ApiError(
-                HttpStatus.BAD_REQUEST,
-                "Incorrectly made request.",
-                e.getBindingResult().getAllErrors().get(0).getDefaultMessage(),
+                HttpStatus.CONFLICT,
+                "For the requested operation the conditions are not met.",
+                e.getMessage(),
                 Collections.emptyList(),
                 LocalDateTime.now()
         );
     }
-}
+
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+        public ApiError handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
+            return new ApiError(
+                    HttpStatus.BAD_REQUEST,
+                    "Incorrectly made request.",
+                    e.getBindingResult().getAllErrors().get(0).getDefaultMessage(),
+                    Collections.emptyList(),
+                    LocalDateTime.now()
+            );
+        }
+    
+        @ExceptionHandler
+        @ResponseStatus(HttpStatus.BAD_REQUEST)
+        public ApiError handleMissingServletRequestParameterException(final MissingServletRequestParameterException e) {
+            return new ApiError(
+                    HttpStatus.BAD_REQUEST,
+                    "Incorrectly made request.",
+                    e.getMessage(),
+                    Collections.emptyList(),
+                    LocalDateTime.now()
+            );
+        }
+    }
+    
